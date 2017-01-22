a1aa5d741e3a61d9e069137022f9586acf71a9eb
David Wallin <dedoubleyou1@gmail.com>
2017-01-22 15:25:46 -0500

    Multiplayer?

--- BouncyBalls/Assets/Scripts/PlayerController.cs
+++ BouncyBalls/Assets/Scripts/PlayerController.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
+	public string verticalName;
+	public string horizontalName;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +14,10 @@ public class PlayerController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		var x = Input.GetAxis (horizontalName) * Time.deltaTime * 3.0f;
+		var z = Input.GetAxis (verticalName) * Time.deltaTime * 3.0f;
+
+		//transform.Rotate (0, x, 0);
+		transform.Translate (x, 0, z);
 	}
 }
